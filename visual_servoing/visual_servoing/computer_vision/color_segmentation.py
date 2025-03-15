67fb48a329f9514badda77626b6153cc8a899659
inimai-s <inimai@mit.edu>
2025-03-15 04:48:17 +0000

    module 1

--- visual_servoing/visual_servoing/computer_vision/color_segmentation.py
+++ visual_servoing/visual_servoing/computer_vision/color_segmentation.py
@@ -36,6 +36,19 @@ def cd_color_segmentation(img, template):
 
 	bounding_box = ((0,0),(0,0))
 
+	# optimized implementation
+	hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+	lower_orange_bound = np.array([0, 200, 102]) #BEST 0.88221
+	upper_orange_bound = np.array([30, 255, 255])
+	mask = cv2.inRange(hsv_img, lower_orange_bound, upper_orange_bound)
+		
+	contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	if contours:
+		largest_contour = max(contours, key=cv2.contourArea)
+		x, y, w, h = cv2.boundingRect(largest_contour)
+		bounding_box = ((x, y), (x + w, y + h))
+
 	########### YOUR CODE ENDS HERE ###########
 
 	# Return bounding box
